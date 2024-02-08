46ac101f28d6ecc36f01e5e48cb22cd507b4877c
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-02-09 04:28:39 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SearchIcon from '@mui/icons-material/Search';
+import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='nav-menu'>
+            <div className='nav-select'>
+                
+            </div>
+            <div className='nav-links'>
+                <Link to={'/myaccount'}> My Account </Link> <p>|</p> 
+                <Link to={'/checkout'}> Checkout </Link><p>|</p>
+                <Link to={'/login'}> Sign In </Link>
+            </div>            
+        </div>
+        <div className='nav-title'>
+            <div className='nav-search'>
+                <input type='text' placeholder='search for catalog'/>
+                <button><SearchIcon /></button>                
+            </div>
+            <div className='title'>
+                <h1>WatchNeedle</h1>
+            </div>
+            <div className='nav-cart'>
+                <LocalMallOutlinedIcon />
+                <p>0</p>
+                <h2>My Cart</h2>
+                <p>$0.00</p>
+            </div>
+        </div>
+        <div className='menu'>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/watch'}>Watch</Link> 
+            <Link to={'/digital-watch'}>Digital Watches</Link>            
+            <Link to={'/smart-watch'}>Smart Watches</Link>            
+            <Link to={'/top-seller'}>Top Seller</Link> 
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
