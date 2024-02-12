56ef942504f337fad0e0e58eeed792e645e00a59
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-02-12 22:10:52 +0530

    e-comerce

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,8 @@
-import Slide from '../components/Slide'
-import slide1 from "../asset/slide1.png"
-import slide2 from "../asset/slide2.png"
 
 const Home = () => {
-
-  const Slides = [
-    {"src" : slide1, "alt" : slide1},
-    {"src" : slide2, "alt" : slide2},
-  ]
-
   return (
-    <div className='home'>
-
-      <div className='slides'>
-        <div>
-          <Slide  slide={Slides}/>
-        </div>
-      </div>
-      
+    <div className='home-container'>
+      Hello World
     </div>
   )
 }
